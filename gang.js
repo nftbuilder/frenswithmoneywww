50bb471dfe5043809200ba844d802821c9ed7b3b
r00t <idontcare@gmail.com>
2022-04-04 10:28:26 +0200

    Update gang.js

--- gang.js
+++ gang.js
@@ -2,7 +2,7 @@ console.clear();
 console.log('lsakdfalskjdflnksd');
 
 const config = {
-  src: 'https://i.ibb.co/Qf0m26z/open-peeps-sheet.png',
+  src: 'https://i.imgur.com/cj137xN.png',
   rows: 15,
   cols: 7 };
 
